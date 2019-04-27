a00c7bd85a472d4248db8e920fb7edcd909e80c3
RobertSamoilescu <robert.samoilescu@gmail.com>
2019-04-27 21:55:41 +0300

    smaller model

--- test_loader.py
+++ test_loader.py
@@ -51,7 +51,52 @@ class MyModel(nn.Module):
         return x
 
 
-model = MyModel(181)
+class MySecondModel(nn.Module):
+    def __init__(self, no_outputs):
+        super(MySecondModel, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 24, (5, 5), padding=2),
+            nn.BatchNorm2d(24),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(24, 36, (5, 5), padding=2),
+            nn.BatchNorm2d(36),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(36, 48, (5, 5), padding=2),
+            nn.BatchNorm2d(48),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(48, 64, (3, 3), padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+
+            nn.Conv2d(64, 64, (3, 3), padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(64 * 5 * 20, 1024),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(512, no_outputs)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.classifier(x)
+        return x
+
+model = MySecondModel(181)
 model = model.cuda()
 print(model)
 
