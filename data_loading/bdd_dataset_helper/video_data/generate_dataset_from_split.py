d0df37835ce2a87bb5b31d04af72e8f8cf6a30a5
Andrei Mihalea <mihalea.andrei@gmail.com>
2021-01-22 17:50:29 +0200

    Working at splitting the dataset

--- data_loading/bdd_dataset_helper/video_data/generate_dataset_from_split.py
+++ data_loading/bdd_dataset_helper/video_data/generate_dataset_from_split.py
@@ -12,8 +12,6 @@ test_split_file = ''
 
 def split_data(args):
     info_files = os.listdir(args.info_dir)
-    train_len = int(TRAIN * len(info_files))
-    valid_len = int(VALID * len(info_files))
 
     video_dict = {}
 
@@ -36,35 +34,44 @@ def split_data(args):
     with open(args.train_split, 'r') as f:
         train_names = f.readlines()
 
-    print(train_names)
-    print(a)
+    # trim the newline from the end
+    train_names = [x[:-1] for x in train_names]
 
-    train_inds = sorted(random.sample(range(len(info_files)), train_len))
+    train_len = int(TRAIN * len(train_names))
+    valid_len = int(VALID * len(train_names))
+
+    train_inds = sorted(random.sample(range(len(train_names)), train_len))
     for ind in reversed(train_inds):
-        vid_name = info_files[ind].split('.')[0] + ".mov"
+        vid_name = train_names[ind] + ".mov"
         print(vid_name)
-        info_file = os.path.join(args.info_dir, info_files[ind])
-        vid_file = video_dict[info_files[ind]]
+        info_file = os.path.join(args.info_dir, train_names[ind])
+        vid_file = video_dict[train_names[ind]]
 
         copy(info_file, os.path.join(args.destination, 'train/info/', info_files[ind]))
         copy(vid_file, os.path.join(args.destination, 'train/data/', vid_name))
 
         #del info_files[ind]
 
-    valid_inds = list(set(range(len(info_files))).difference(set(train_inds)))
+    valid_inds = list(set(range(len(train_names))).difference(set(train_inds)))
     for ind in reversed(valid_inds):
-        vid_name = info_files[ind].split('.')[0] + ".mov"
+        vid_name = train_names[ind] + ".mov"
         info_file = os.path.join(args.info_dir, info_files[ind])
-        vid_file = video_dict[info_files[ind]]
+        vid_file = video_dict[train_names[ind]]
 
         copy(info_file, os.path.join(args.destination, 'validation/info/', info_files[ind]))
         copy(vid_file, os.path.join(args.destination, 'validation/data/', vid_name))
         #del info_files[ind]
 
-    for file in info_files:
-        vid_name = file.split('.')[0] + ".mov"
+    with open(args.test_split, 'r') as f:
+        test_names = f.readlines()
+
+    # trim the newline from the end
+    test_names = [x[:-1] for x in test_names]
+
+    for file in test_names:
+        vid_name = file.split + ".mov"
         info_file = os.path.join(args.info_dir, file)
-        vid_file = video_dict[info_files[ind]]
+        vid_file = video_dict[file]
 
         copy(info_file, os.path.join(args.destination, 'test/info/', info_files[ind]))
         copy(vid_file, os.path.join(args.destination, 'test/data/', vid_name))
